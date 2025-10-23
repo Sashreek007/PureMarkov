c78d440efd22fdb319b4cb781a03202bc65ba0c5
Sashreek007 <Sashreek.addanki@gmail.com>
2025-10-23 16:27:39 -0600

    wrote an algorithm to train(statistically)

--- src/markov_chain.py
+++ src/markov_chain.py
@@ -57,3 +57,51 @@ class Markovchain:
         words = text.split()
 
         return words
+
+    def train(self, text):
+        """
+        This is not exactly machine learning way of training. Its more like a count-based statistical training
+
+        1. We tokenize the text into words useing the function created
+        2. For each word record the next word in the dict created with the count, increment it
+        3. Add words to the vacbulary set
+
+        Args:
+            text(str): training text
+
+        Example:
+            If we train on "the cat sat on the bed":
+            - "the"-> "cat" (count:1)
+            - "cat" -> "sat" (count:1)
+            - "sat" -> "on" (count:1)
+            - "on" -> "the" (count:1)
+            - "the" -> "bed" (count:1)
+
+        self.transitions would look like:
+            {
+                "the": {"cat": 1, "mat": 1},
+                "cat": {"sat": 1},
+                "sat": {"on": 1},
+                "on": {"the": 1},
+                "bed": {}
+
+            }
+        """
+
+        # Tokenize
+        words = self.tokenize((text))
+
+        for i in range(len(words) - 1):
+            currentWord = words[i]
+            nextWord = words[i + 1]
+
+            self.transitions[currentWord][nextWord] += 1
+            # the number of times we saw a new word
+            self.transistionCount += 1
+
+            self.vocabulary.add(currentWord)
+            self.vocabulary.add(nextWord)
+
+        print(f"Trained on {len(words)} words")
+        print(f"Vocabulary size: {len(self.vocabulary)}")
+        print(f"Unique transitions learned: {len(self.transitions)}")
